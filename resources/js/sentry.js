1bf88a9144e469bd466c7a04d99e032adb729cd4
curder <curder@foxmail.com>
2020-06-23 15:18:53 +0800

    webpack sentry support

--- resources/js/sentry.js
+++ resources/js/sentry.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import * as Sentry from '@sentry/browser';
 import * as Integrations from '@sentry/integrations';
 
+// console.log(process.env.MIX_SENTRY_FRONTEND_DSN);
+// console.log(process.env.MIX_SENTRY_RELEASE);
+// console.log(process.env.MIX_APP_ENVIRONMENT);
+
 Sentry.init({
-    dsn: process.env.SENTRY_DNS,
+    dsn: process.env.MIX_SENTRY_FRONTEND_DSN,
     integrations: [
     new Integrations.Vue({
       Vue,
@@ -15,8 +19,8 @@ Sentry.init({
       }
     })
   ],
-  release: process.env.SENTRY_RELEASE,
-  environment: process.env.NODE_ENV,
+  release: process.env.MIX_SENTRY_RELEASE,
+  environment: process.env.MIX_APP_ENVIRONMENT,
 });
 
 // 在捕获错误的界面需要用到Sentry
--- webpack.mix.js
+++ webpack.mix.js
@@ -19,31 +19,40 @@ mix.js('resources/js/app.js', 'public/js')
 
 const gitSha = require('child_process').execSync('git rev-parse HEAD').toString().trim()
 
+
 mix.webpackConfig(webpack => {
     return {
-        devtool: mix.inProduction() ? 'source-map' : false,
         plugins: [
             new webpack.DefinePlugin({ // 设置环境变量信息
-              "process.env.SENTRY_DNS": process.env.MIX_SENTRY_FRONTEND_DSN, // the value from .env file, see: https://laravel.com/docs/7.x/mix
-              "process.env.SENTRY_RELEASE": gitSha,
+              "process.env.MIX_SENTRY_FRONTEND_DSN": process.env.MIX_SENTRY_FRONTEND_DSN, // the value from .env file, see: https://laravel.com/docs/7.x/mix
+              "process.env.MIX_SENTRY_RELEASE": JSON.stringify(gitSha),
+              "process.env.MIX_APP_ENVIRONMENT": JSON.stringify(process.env.APP_ENV),
             }),
-            () => mix.inProduction() && process.env.MIX_SENTRY_ENABLED ?
-                    new SentryCliPlugin({
-                              include: './public/js',
-                              ignoreFile: '.sentrycliignore',
-                              ignore: ['node_modules', 'webpack.config.js', 'webpack.mix.js'],
-                              configFile: 'sentry.properties',
-                              urlPrefix: "~/js",
-                              dryRun: false,
-                              debug: true,
-                              release: gitSha, // process.env.SENTRY_RELEASE,
-                              deleteAfterCompile: true,
-                              setCommits: {
-                                // repo: "http://github.com/curder/laravel-sentry",
-                                // commit: gitSha, // JSON.stringify(process.env.SENTRY_RELEASE),
-                              }
-                            }) :
-                    false,
         ]
     };
 });
+
+if (process.env.MIX_SENTRY_FRONTED_ENABLED && mix.inProduction()) {
+    mix.webpackConfig(webpack => {
+        return {
+            devtool: 'source-map',
+            plugins: [
+                new SentryCliPlugin({
+                  include: './public/js',
+                  ignoreFile: '.sentrycliignore',
+                  ignore: ['node_modules', 'webpack.config.js', 'webpack.mix.js'],
+                  configFile: 'sentry.properties',
+                  urlPrefix: "~/js",
+                  dryRun: false,
+                  debug: false, // 是否开启调试
+                  release: gitSha, // process.env.SENTRY_RELEASE,
+                  deleteAfterCompile: true,
+                  setCommits: {
+                    // repo: "http://github.com/curder/laravel-sentry",
+                    // commit: gitSha, // JSON.stringify(process.env.SENTRY_RELEASE),
+                  }
+                }),
+            ],
+        }
+    });
+}
